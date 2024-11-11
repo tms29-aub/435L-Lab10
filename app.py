3016104a1493cc25c9b89f1c65f6a9cc020b9597
tamersafa <tamersafa@outlook.com>
2024-11-11 17:12:03 +0200

    modified files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 def greet(name):
     return f"Hello, {name}!"
 
-if __name__ == "__main__": print(greet("World"))
+
+if __name__ == "__main__":
+    print(greet("World"))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,14 @@
-# tests/test_app.py 
+# tests/test_app.py
 import unittest
 from app import greet
 
+
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(
+            greet("World"), "Hello, World from FirstName LastName!"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
