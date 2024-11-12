3b8b760322fbf6d735ecae2a23b8ffb36daedc10
tamersafa <tamersafa@outlook.com>
2024-11-12 11:28:05 +0200

    fixed app and test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello, World from {name}!"
 
 
 if __name__ == "__main__":
-    print(greet("World"))
+    print(greet("Tamer Safa"))
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from app import greet
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(
-            greet("World"), "Hello, World from FirstName LastName!"
+            greet("Tamer Safa"), "Hello, World from Tamer Safa!"
         )
 
 
